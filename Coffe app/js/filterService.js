071d36eeba91bf9d23d84317d11c4f0d5d13cb54
Kevin Tom Agerbo Nicholas <nicholas.kevin96@gmail.com>
2020-02-24 19:37:06 +0100

    fundet en bug
    fix imorgen

--- Coffe app/js/filterService.js
+++ Coffe app/js/filterService.js
@@ -1,8 +1,7 @@
 import { firebaseDB } from "./firebase.js";
-import { beaconsService } from "./beaconService.js";
 
 
-
+// variabler til de forskellige funktioner længere nede
 let aktivitoplag;
 let searchGroup = [];
 
@@ -18,6 +17,8 @@ export class filterService {
   }
 
   readfilters() {
+
+
     // watch the database ref for changes snapshot til at henter opslag fra vores firebeacon
     this.opslag.onSnapshot(snapshotData => {
       let beacon = [];
@@ -33,15 +34,14 @@ export class filterService {
   }
 
   filterBeacon(beacons) {
+
     /// få values fra input field
     let searchInput = document.querySelector("#inputvalue").value;
     /// tom array til push af filtreret
     searchGroup = [];
 
     let htmlTemplate = "";
-
-
-    // for loop for at gå igennem arrayet og render til dommen
+    // for loop for at gå igennem arrayet og sætter alle beacons med proterty .name til små bogstaver.  
     for (var i = 0; i < beacons.length; i++) {
       let filterinput = beacons[i].name.toLowerCase();
 
@@ -52,10 +52,9 @@ export class filterService {
       // Kigger efter op input value fra søge felt er det samme som navnet i arrayet, hvis der så render via dommen.
       if (filterinput.includes(searchInput.toLowerCase())) {
 
-        /// push beacon efter filteringen ind i et nyt array kaldet searchgroup
 
-        ////
 
+        /// laver en html template for at render til dommen i forloopet
 
         htmlTemplate += `
               <div class="opslagwrap">
@@ -70,41 +69,49 @@ export class filterService {
               </div>
               </div>
               `;
+        /// push beacon efter filteringen ind i et nyt array kaldet searchgroup
         searchGroup.push(beacons[i]);
 
 
       }
 
-      //find elementet at render til
+      //find elementet at render til i vores html opsætning
       document.getElementById("alleopslag").innerHTML = htmlTemplate;
       aktivitoplag = document.getElementsByClassName("opslagwrap");
-      this.klikbareOpslag();
+
+      /// gør klar til at at væres klikfunktion kan køres
+      this.klikOpslag();
 
 
 
     }
   }
 
-  klikbareOpslag() {
-    ///tænk på hvor this id referer til.
+  klikOpslag() {
+
+
+
+    ///klikfunktionen defineres
 
     /// htmlpop for at lave en tom html template at render til
     let htmlTemplatepopup = "";
     for (var i = 0; i < aktivitoplag.length; i++) {
 
-      /// sæt aktivt class id proptery til at være indexet i arrayet.
+      /// sæt aktivt class id proptery til at være indexet i arrayet. Så vi har indexeret de bestemte objekter i arrayet
       aktivitoplag[i].id = i;
 
       /// eventlisner til at se høre efter click i arryet  
       aktivitoplag[i].addEventListener("click", function () {
 
         console.log(this.id)
+        ///gør klar til at render vores informationer i objekterne til dommen
         htmlTemplatepopup += `
         <div id="klikkedopslagoverlaywrap">
         <div id="userwrapstuff">
         <div id="resetknap"></div>
           <div id="Oimgwrap">
             <div id="placeimg" style="background-image: url('${
+          /// referer til de specifikke id på objektet i dommen
           searchGroup[this.id].img
           }')"></div>
           </div>
@@ -113,6 +120,7 @@ export class filterService {
           </div>
           <div id="OpslagsInf">
               <div id="OPImg" style="background-image: url('${
+          /// referer til de specifikke id på objektet i dommen
           searchGroup[this.id].profilimg
           }')"></div>
               <div id="OPInf">
@@ -133,12 +141,15 @@ export class filterService {
         </div>
       </div>
         `;
+        /// find alle opslag i vores html
         document.getElementById("alleopslag").innerHTML = htmlTemplatepopup;
 
+        /// lav en reset knap for at fjerne de objecter som er blevet renderet i html
         document.getElementById("resetknap").addEventListener("click", function () {
           /// render ingenting til dommet og derefter udskift med html pop, kald new filterservice for at køre classen igen.
           htmlTemplatepopup = "";
           document.getElementById("alleopslag").innerHTML = htmlTemplatepopup;
+          ///kald fiilterserive for at sparke gang i funktionerne forfra, så vi ikke har en tom side.
           new filterService;
         });
       });
@@ -147,4 +158,4 @@ export class filterService {
 }
 
 
-new filterService();
+new filterService;
